55eb7996d7f28fac482c8ffff0dcb1561ec929d2
Baptiste Paqueriaud <paqueriaudbaba@gmail.com>
2023-01-19 21:54:19 +0000

    updated to 0.5

--- Formula/mkpj.rb
+++ Formula/mkpj.rb
@@ -1,9 +1,9 @@
 class Mkpj < Formula
     desc "A simple, yet efficient C++ project assistant."
     homepage "https://github.com/BaptisteP31/mkpj"
-    url "https://github.com/BaptisteP31/mkpj/releases/download/0.4/mkpj"
-    sha256 "ec51135764737f8b4e9b046106dbe8bc3abdf4104dca7c3c9afb4f7a22541e31"
-    version "0.4.0"
+    url "https://github.com/BaptisteP31/mkpj/releases/download/0.5/mkpj"
+    sha256 "f5a451eb21d81db31a666b2383a9b20127964ff805d4e364dd871702d5d84309"
+    version "0.5.0"
 
     def install
         bin.install "mkpj"
