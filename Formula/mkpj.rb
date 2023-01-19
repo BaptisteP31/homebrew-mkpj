a3243d049bdc64e6d35a0d8e53014b457154b4bc
Baptiste Paqueriaud <paqueriaudbaba@gmail.com>
2023-01-19 13:49:18 +0000

    added for

---
+++ Formula/mkpj.rb
@@ -0,0 +1,12 @@
+class Espanso < Formula
+    desc "A simple, yet efficient C++ project assistant."
+    homepage "https://github.com/BaptisteP31/mkpj"
+    url "https://github.com/BaptisteP31/mkpj/archive/refs/tags/0.4.tar.gz"
+    sha256 "2dc1c234afdcb471e5fb0bb4a927e0bd51203d0ae45070da4f2738f2e06ab659"
+    version "0.4.0"
+
+    def install
+        system "make", "configure"
+        system "sudo", "make", "install"
+    end
+end
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # homebrew-mkpj
+
+This is a [Homebrew](https://brew.sh/) tap for [mkpj](https://github.com/BaptisteP31/mkpj).
