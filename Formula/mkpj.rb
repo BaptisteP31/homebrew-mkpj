b217a5ead3bbcedfd4eb611a8e32d3b8e781e256
Paqueriaud Baptiste <paqueriaudbaba@gmail.com>
2023-02-07 10:43:49 +0100

    moved to 0.7

--- Formula/mkpj.rb
+++ Formula/mkpj.rb
@@ -1,9 +1,9 @@
 class Mkpj < Formula
     desc "A simple, yet efficient C++ project assistant."
     homepage "https://github.com/BaptisteP31/mkpj"
-    url "https://github.com/BaptisteP31/mkpj/releases/download/0.6/mkpj"
-    sha256 "347a73adbb5d2f4fdc1aa8d4ec2dde8e96ba8e8b4132800b9bee879584bf8de3"
-    version "0.6.1"
+    url "https://github.com/BaptisteP31/mkpj/releases/download/0.7/mkpj"
+    sha256 "24e710781ae394481d3f265ba9d46a30882832f9e2e7f41718b1ddb5a47e9fec"
+    version "0.7.0"
 
     def install
         bin.install "mkpj"
